bd8547992c3fca86580887e769a5275f597219aa
Jhony Burbano <jhonyburbanojb@gmail.com>
2018-07-24 11:24:53 -0500

    Creación y agregación de migración y tablas (Productos, Categorias)

---
+++ database/migrations/2018_07_24_161149_create_productos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('categoria_id')->nullable();
+            $table->foreign('categoria_id')->references('id')->on('categoria_id');
+
+            $table->string('nombre', 150);
+            $table->string('descripcion')->nullable();
+            $table->text('descripcion_larga')->nullable();
+            $table->double('precio');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
